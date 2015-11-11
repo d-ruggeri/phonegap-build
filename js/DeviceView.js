1357773570a0ba330a66b7e41483ec267b807be2
unknown <a138571@E0523651.risorse.enel>
2015-11-11 13:40:09 +0100

    access origin *

--- js/DeviceView.js
+++ js/DeviceView.js
@@ -7,12 +7,74 @@ var DeviceView = function(device) {
     this.render = function() {
         var $html = $(this.template(device));
         this.$el.html($html);
-        setTimeout(this.createGraph, 1000);
+        setTimeout(this.createGraph1, 1000);
         return this;
     };
     
-    this.createGraph = function() {
-        var ctx = $('.grafico.active').find('canvas').get(0).getContext("2d");
+    this.createGraph1 = function() {
+        var ctx = $('#chart1mobile').find('canvas').get(0).getContext("2d");
+        var options = {
+            scaleBeginAtZero : true,
+            scaleShowGridLines : true,
+            scaleGridLineColor : "rgba(0,0,0,.05)",
+            scaleGridLineWidth : 1,
+            scaleShowHorizontalLines: true,
+            scaleShowVerticalLines: true,
+            barShowStroke : true,
+            barStrokeWidth : 2,
+            barValueSpacing : 5,
+            barDatasetSpacing : 1,
+            legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+        };           
+        var data = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fillColor: "rgba(220,220,220,0.5)",
+                    strokeColor: "rgba(220,220,220,0.8)",
+                    highlightFill: "rgba(220,220,220,0.75)",
+                    highlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                }
+            ]
+        };
+        var myNewChart = new Chart(ctx).Bar(data, options);
+    }
+    
+    this.createGraph2 = function() {
+        var ctx = $('#chart2mobile').find('canvas').get(0).getContext("2d");
+        var options = {
+            scaleBeginAtZero : true,
+            scaleShowGridLines : true,
+            scaleGridLineColor : "rgba(0,0,0,.05)",
+            scaleGridLineWidth : 1,
+            scaleShowHorizontalLines: true,
+            scaleShowVerticalLines: true,
+            barShowStroke : true,
+            barStrokeWidth : 2,
+            barValueSpacing : 5,
+            barDatasetSpacing : 1,
+            legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+        };           
+        var data = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fillColor: "rgba(220,220,220,0.5)",
+                    strokeColor: "rgba(220,220,220,0.8)",
+                    highlightFill: "rgba(220,220,220,0.75)",
+                    highlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                }
+            ]
+        };
+        var myNewChart = new Chart(ctx).Bar(data, options);
+    }
+
+    this.createGraph3 = function() {
+        var ctx = $('#chart3mobile').find('canvas').get(0).getContext("2d");
         var options = {
             scaleBeginAtZero : true,
             scaleShowGridLines : true,
